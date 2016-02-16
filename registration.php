8589fd74a6721c4608d1a34889983091b24e4bf1
stevew <stevew@bucks.net>
2016-02-16 11:27:28 +0000

    First changes to test translation
    en_GB working files for luma theme

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Magento2 British Language Pack (en_GB)
+ *
+ * Website: http://bnspayments.uk/magento2-language-en-gb
+ *
+ * @author Steve Walker <stevew@bnspayments.uk>
+ * @copyright Copyright 2016 bns
+ * @version 100.1.1
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::LANGUAGE, 'bns_en_gb', __DIR__
+);
